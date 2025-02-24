b8659cd291dce1824040099dd13fa4d433550634
kiqiou <kuksare@gmail.com>
2025-02-24 19:59:52 +0300

    Initial commit

---
+++ android/app/src/main/java/com/example/amdby_shop/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.amdby_shop;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/image/banner_image.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoundedImage extends StatelessWidget {
+  final double? width, height;
+  final String imageUrl;
+  final bool applyImageRadius;
+  final BoxBorder? border;
+  final Color backgroundColor;
+  final BoxFit? fit;
+  final EdgeInsetsGeometry? padding;
+  final bool isNetworkImage;
+  final VoidCallback? onPressed;
+  final double borderRadius;
+
+  const RoundedImage({
+    super.key,
+    this.border,
+    this.padding,
+    this.width,
+    this.height,
+    required this.imageUrl,
+    this.applyImageRadius = true,
+    this.backgroundColor = Colors.white38,
+    this.fit = BoxFit.contain,
+    this.isNetworkImage = false,
+    this.onPressed,
+    this.borderRadius = 25.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+          width: width,
+          height: height,
+          padding: padding,
+          decoration: BoxDecoration(
+            border: border,
+            color: backgroundColor,
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+          child: ClipRRect(
+            borderRadius: applyImageRadius ? BorderRadius.circular(borderRadius) : BorderRadius.zero,
+            child: Image(
+              fit: fit,
+              image: isNetworkImage ? NetworkImage(imageUrl) : AssetImage(imageUrl) as ImageProvider,
+            ),
+          )),
+    );
+  }
+}
