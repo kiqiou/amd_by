6cded3db5b5cc6268a94569f5dbac320f53d82f9
kiqiou <kuksare@gmail.com>
2025-03-18 08:25:16 +0300

    feat: add product characteristic

--- lib/screens/home_screen/views/details_screen.dart
+++ lib/screens/home_screen/views/details_screen.dart
@@ -91,7 +91,14 @@ class DetailsScreen extends StatelessWidget {
                     SizedBox(
                       height: spaceBetween,
                     ),
-                    RowWithIcon(icon: Icons.add_alert_sharp, text: "Узнать о снижении цены", color: Colors.blueAccent, onTap: () {print("Нажатие");},),
+                    RowWithIcon(
+                      icon: Icons.add_alert_sharp,
+                      text: "Узнать о снижении цены",
+                      color: Colors.blueAccent,
+                      onTap: () {
+                        print("Нажатие");
+                      },
+                    ),
                     SizedBox(
                       height: spaceBetween,
                     ),
@@ -99,24 +106,34 @@ class DetailsScreen extends StatelessWidget {
                     SizedBox(
                       height: spaceBetween,
                     ),
-                    RowWithIcon(icon: Icons.monetization_on_rounded, text: "Бонусные баллы за покупку", color: Colors.purple),
+                    RowWithIcon(
+                        icon: Icons.monetization_on_rounded, text: "Бонусные баллы за покупку", color: Colors.purple),
                     Divider(
                       color: Colors.black12,
                     ),
                     SizedBox(
                       height: spaceBetween,
                     ),
-                    RowWithIcon(icon: Icons.directions_car_filled, text: "Доставка в г.Минск 19 марта с 18:00 до 23:00. Стоимость: БЕСПЛАТНО", color: Colors.black45),
+                    RowWithIcon(
+                        icon: Icons.directions_car_filled,
+                        text: "Доставка в г.Минск 19 марта с 18:00 до 23:00. Стоимость: БЕСПЛАТНО",
+                        color: Colors.black45),
                     SizedBox(
                       height: 5.0,
                     ),
-                    RowWithIcon(icon: Icons.local_shipping, text: "Доставка в пункт самовывоза (г.Минск) 19 марта", color: Colors.black45),
+                    RowWithIcon(
+                        icon: Icons.local_shipping,
+                        text: "Доставка в пункт самовывоза (г.Минск) 19 марта",
+                        color: Colors.black45),
                     SizedBox(
                       height: spaceBetween,
                     ),
                     RowWithIcon(icon: Icons.gpp_good_outlined, text: "Гарантия: 12 месяцев", color: Colors.blueGrey),
                     SizedBox(
                       height: spaceBetween,
+                    ),
+                    Divider(
+                      color: Colors.black12,
                     ),
                     Align(
                       alignment: Alignment.centerLeft,
@@ -128,6 +145,37 @@ class DetailsScreen extends StatelessWidget {
                         ),
                       ),
                     ),
+                    SizedBox(
+                      height: spaceBetween,
+                    ),
+                    SizedBox(
+                      height: 500.0,
+                      child: ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Row(
+                            children: [
+                              Flexible(
+                                child: Text('Название характеристики'),
+                              ),
+                              VerticalDivider(
+                                color: Colors.black12,
+                                width: 2,
+                                thickness: 1,
+                                indent: 10,
+                                endIndent: 10,
+                              ),
+                              Flexible(
+                                child: Text('Сама характеристика'),
+                              ),
+                              Divider(
+                                color: Colors.black12,
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    )
                   ],
                 ),
               ),
@@ -150,22 +198,22 @@ class RowWithIcon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
-        onTap: onTap,
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Row(
-            children: [
-              Icon(icon, color: color, size: 18),
-              SizedBox(width: 5),
-              Expanded(
-                child: Text(
-                  text,
-                  style: TextStyle(fontSize: 16, color: color),
-                ),
+      onTap: onTap,
+      child: Align(
+        alignment: Alignment.centerLeft,
+        child: Row(
+          children: [
+            Icon(icon, color: color, size: 18),
+            SizedBox(width: 5),
+            Expanded(
+              child: Text(
+                text,
+                style: TextStyle(fontSize: 16, color: color),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
     );
   }
 }
